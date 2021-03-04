65953fc8a44c2f643d35baf20b65b5c8136c037e
Jacob Voyles <jtvkw2@gmail.com>
2021-03-04 17:00:13 -0600

    Added Window and FPS Label

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,46 @@
-import pyglet
-
-window = pyglet.window.Window()
-label = pyglet.text.Label('Hello, world!',
-                          font_size=36,
-                          x=window.width // 2,
-                          y=window.height // 2,
-                          anchor_x='center',
-                          anchor_y='center')
+from pyglet import image
+from pyglet.gl import *
+from pyglet.graphics import TextureGroup
+from pyglet.window import key, mouse
 
 
-@window.event
-def on_draw():
-    window.clear()
-    label.draw()
+class Window(pyglet.window.Window):
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args, **kwargs)
+        self.exclusive = False
+        self.label = pyglet.text.Label('', font_name='Arial', font_size=18,
+            x=10, y=self.height - 10, anchor_x='left', anchor_y='top',
+            color=(0, 0, 0, 255))
+        pyglet.clock.schedule_interval(60,60)
+
+    def set_exclusive_mouse(self, exclusive):
+        """ If `exclusive` is True, the game will capture the mouse, if False
+        the game will ignore the mouse.
+
+        """
+        super(Window, self).set_exclusive_mouse(exclusive)
+        self.exclusive = exclusive
+
+    def on_draw(self):
+        self.clear()
+        self.draw_label()
+
+    def draw_label(self):
+        self.label.text = '%02d' % (
+            pyglet.clock.get_fps())
+        self.label.draw()
 
 
-pyglet.app.run()
+def setup():
+    glClearColor(0.5, 0.69, 1.0, 1)
+    glEnable(GL_CULL_FACE)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+
+def main():
+    window = Window(width=1280, height=720, caption='Game', resizable=True)
+    window.set_exclusive_mouse(True)
+    setup()
+    pyglet.app.run()
+
+main()
